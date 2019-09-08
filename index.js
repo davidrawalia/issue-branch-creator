24fc4d81d29e78e4822597dcedcd8a8689f7aeb6
David Rawalia <davidrawalia@gmail.com>
2019-09-07 23:45:54 -0400

    implement automated branch creation with issue label

--- index.js
+++ index.js
@@ -1,19 +1,56 @@
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Application} app
- */
 module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
+  app.log('App was loaded')
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
+  app.on('issues.labeled', async context => {
+    app.log('Issue was opened')
+    const owner = context.payload.repository.owner.login
+    const repo = context.payload.repository.name
+    const issueNumber = context.payload.issue.number
+    const issueTitle = context.payload.issue.title
+    const issueLabel = context.payload.label.name
+    app.log(`Issue label: ${issueLabel}`)
+    const branchName = await getBranchNameFromIssue(context, issueNumber, issueTitle, issueLabel)
+    if (await branchExists(context, owner, repo, branchName)) {
+      app.log('Branch already exists')
+    } else {
+      const sha = await getDefaultBranchHeadSha(context, owner, repo)
+      await createBranch(context, owner, repo, branchName, sha)
+      app.log(`Branch created: ${branchName}`)
+    }
   })
+}
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+async function branchExists (context, owner, repo, branchName) {
+  try {
+    await context.github.gitdata.getRef({
+      'owner': owner, 'repo': repo, ref: `heads/${branchName}`
+    })
+    return true
+  } catch (err) {
+    return false
+  }
+}
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+async function getDefaultBranchHeadSha (context, owner, repo) {
+  const defaultBranch = context.payload.repository.default_branch
+  const res = await context.github.gitdata.getRef({
+    owner: owner, repo: repo, ref: `heads/${defaultBranch}`
+  })
+  const ref = res.data.object
+  return ref.sha
+}
+
+async function createBranch (context, owner, repo, branchName, sha) {
+  const res = await context.github.gitdata.createRef({
+    'owner': owner, 'repo': repo, 'ref': `refs/heads/${branchName}`, 'sha': sha
+  })
+  return res
+}
+
+async function getBranchNameFromIssue (context, number, title, label) {
+  let branchTitle = title.replace(/[\W]+/g, '_')
+  if (branchTitle.endsWith('_')) {
+    branchTitle = branchTitle.slice(0, -1)
+  }
+  return `${label}-${number}-${branchTitle}`
 }
